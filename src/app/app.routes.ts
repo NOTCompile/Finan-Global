b98c1dd39fe52f955222c11f7dc7cee75f73698b
NOTCompile <notcompile@gmail.com>
2025-09-29 00:41:39 -0500

    Sesion-Page(Css - Html)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'inicio-sesion',
+        loadComponent: () => import('./pages/sesion-page/sesion-page.component'),
+    },
+    {
+        path:'dashboard',
+        loadComponent: () => import('./pages/dashboard-page/dashboard-page.component'),
+    },
+    {
+        path: '**',
+        redirectTo: 'inicio-sesion'
+    }
+];
